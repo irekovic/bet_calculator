251b21ab48ec2faf8e30392cfab509ce0d7d22bf
Ivan Rekovic <irekovic@gmail.com>
2015-08-26 19:09:46 +0200

    Refactoring

--- lib/bet_calculator/bet_types.rb
+++ lib/bet_calculator/bet_types.rb
@@ -1,29 +1,45 @@
 module BetCalculator
-
-	def self.full_cover(stake, count, legs)
-		Enumerator.new do |y|
-			legs.combination(count).each do |prices|
-				(2..count).each do |fold|
-					prices.combination(fold).each do |combination|
-						y << MultipleBet.new(stake, combination)
+	module BetHelpers
+		def full_cover(stake, count, legs)
+			Enumerator.new do |y|
+				legs.combination(count).each do |prices|
+					(2..count).each do |fold|
+						prices.combination(fold).each do |combination|
+							y << MultipleBet.new(stake, combination)
+						end
 					end
 				end
 			end
 		end
-	end
 
-	def self.full_cover_with_singles(stake, count, legs)
-		Enumerator.new do |y|
-			legs.combination(count) do |prices|
-				prices.each do |price|
-					y << SingleBet.new(stake, price)
-				end 
-				full_cover(stake, count, prices).each { |bet| y << bet }
+		def full_cover_with_singles(stake, count, legs)
+			Enumerator.new do |y|
+				legs.combination(count) do |prices|
+					prices.each do |price|
+						y << SingleBet.new(stake, price)
+					end 
+					full_cover(stake, count, prices).each { |bet| y << bet }
+				end
+			end
+		end
+
+		def union_jack(elements)
+			Enumerator.new do |y|
+				y << elements.values_at(0,1,2)
+				y << elements.values_at(3,4,5)
+				y << elements.values_at(6,7,8)
+				y << elements.values_at(0,3,6)
+				y << elements.values_at(1,4,7)
+				y << elements.values_at(2,5,8)
+				y << elements.values_at(0,4,8)
+				y << elements.values_at(2,4,6)
 			end
 		end
 	end
 
 	class BetType
+		include BetHelpers
+
 		def initialize(stake, legs)
 			@stake = stake.to_f
 			@prices = legs.to_a
@@ -54,57 +70,57 @@ module BetCalculator
 
 	class Trixie < BetType
 		def bets
-			BetCalculator.full_cover(@stake, 3, @prices)
+			full_cover(@stake, 3, @prices)
 		end
 	end
 
 	class Yankee < BetType
 		def bets
-			BetCalculator.full_cover(@stake, 4, @prices)
+			full_cover(@stake, 4, @prices)
 		end
 	end
 
 	class Canadian < BetType
 		def bets
-			BetCalculator.full_cover(@stake, 5, @prices)
+			full_cover(@stake, 5, @prices)
 		end
 	end
 
 
 	class Heinz < BetType
 		def bets
-			BetCalculator.full_cover(@stake, 6, @prices)
+			full_cover(@stake, 6, @prices)
 		end
 	end
 
 	class SuperHeinz < BetType
 		def bets
-			BetCalculator.full_cover(@stake, 7, @prices)
+			full_cover(@stake, 7, @prices)
 		end
 	end
 
 	class Goliath < BetType
 		def bets
-			BetCalculator.full_cover(@stake, 8, @prices)
+			full_cover(@stake, 8, @prices)
 		end
 	end
 
 
 	class Block < BetType
 		def bets
-			BetCalculator.full_cover(@stake, 9, @prices)
+			full_cover(@stake, 9, @prices)
 		end
 	end
 
 	class Patent < BetType
 		def bets
-			BetCalculator.full_cover_with_singles(@stake, 3, @prices)
+			full_cover_with_singles(@stake, 3, @prices)
 		end
 	end
 
 	class Yap < BetType
 		def bets
-			BetCalculator.full_cover_with_singles @stake, 4, @prices
+			full_cover_with_singles @stake, 4, @prices
 		end
 	end
 
@@ -113,31 +129,31 @@ module BetCalculator
 
 	class Lucky31 < BetType
 		def bets
-			BetCalculator.full_cover_with_singles @stake, 5, @prices
+			full_cover_with_singles @stake, 5, @prices
 		end
 	end
 
 	class Lucky63 < BetType
 		def bets
-			BetCalculator.full_cover_with_singles @stake, 6, @prices
+			full_cover_with_singles @stake, 6, @prices
 		end
 	end
 
 	class SuperHeinzWithSingles < BetType
 		def bets
-			BetCalculator.full_cover_with_singles @stake, 7, @prices
+			full_cover_with_singles @stake, 7, @prices
 		end
 	end
 
 	class GoliathWithSingles < BetType
 		def bets
-			BetCalculator.full_cover_with_singles @stake, 8, @prices
+			full_cover_with_singles @stake, 8, @prices
 		end
 	end
 
 	class BlockWithSingles < BetType
 		def bets
-			BetCalculator.full_cover_with_singles @stake, 9, @prices
+			full_cover_with_singles @stake, 9, @prices
 		end
 	end
 
@@ -252,23 +268,10 @@ module BetCalculator
 		end
 	end
 
-	def self.union_jack(elements)
-		Enumerator.new do |y|
-			y << elements.values_at(0,1,2)
-			y << elements.values_at(3,4,5)
-			y << elements.values_at(6,7,8)
-			y << elements.values_at(0,3,6)
-			y << elements.values_at(1,4,7)
-			y << elements.values_at(2,5,8)
-			y << elements.values_at(0,4,8)
-			y << elements.values_at(2,4,6)
-		end
-	end
-
 	class UnionJackTreble < BetType
 		def bets
 			Enumerator.new do |y|
-				BetCalculator.union_jack(@prices).each do |legs| 
+				union_jack(@prices).each do |legs| 
 					Accumulator.new(@stake, legs).bets.each { |e| y << e } 
 				end
 			end
@@ -278,7 +281,7 @@ module BetCalculator
 	class UnionJackTrixie < BetType
 		def bets
 			Enumerator.new do |y|
-				BetCalculator.union_jack(@prices).each do |legs|
+				union_jack(@prices).each do |legs|
 					Trixie.new(@stake, legs).bets.each { |e| y << e }
 				end
 			end
@@ -288,7 +291,7 @@ module BetCalculator
 	class UnionJackPatent < BetType
 		def bets
 			Enumerator.new do |y|
-				BetCalculator.union_jack(@prices).each do |legs|
+				union_jack(@prices).each do |legs|
 					Patent.new(@stake, legs).bets.each { |e| y << e }
 				end
 			end
@@ -298,11 +301,10 @@ module BetCalculator
 	class UnionJackRoundRobin < BetType
 		def bets
 			Enumerator.new do |y|
-				BetCalculator.union_jack(@prices).each do |legs|
+				union_jack(@prices).each do |legs|
 					RoundRobin.new(@stake, legs).bets.each { |e| y << e }
 				end
 			end
 		end
 	end
-
 end
