066225743427126032d5c53d1f3b4688c26d5183
Ivan Rekovic <irekovic@gmail.com>
2015-08-29 16:06:02 +0200

    bugfix

--- spec/win_only_calculator_spec.rb
+++ spec/win_only_calculator_spec.rb
@@ -1,7 +1,7 @@
 module BetCalculator
   describe WinOnlyCalculator do
     it "calculates singles correctly" do
-      bet = SingleBet.new(10, Leg.new(10, 1))
+      bet = SingleBet.new(10, Leg.new(LegPart.new(10)))
       calculator = BetCalculator::WinOnlyCalculator.new  
       expect(calculator.single(bet).to_a).to contain_exactly Unit.new 10, 20
     end
